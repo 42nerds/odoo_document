351d69732bf7a16e780bf528d570c8da08a69c4e
Fabian Krahtz <fabian.krahtz@live.com>
2020-06-22 14:20:07 +0000

    Rename field and add missing localization

--- models/res_company.py
+++ models/res_company.py
@@ -4,7 +4,7 @@ class Company(models.Model):
     _inherit = 'res.company'
 
     ceos = fields.Char(string='CEOs', store=True, readonly=False)
-    company_registry_id = fields.Char(string='Company Registration ID', store=True, readonly=False)
+    company_registry_id = fields.Char(string='Company Reg No', store=True, readonly=False)
     corporation_location = fields.Char(string='Location of Corporation', store=True, readonly=False)
     fax = fields.Char(string='Fax', store=True, readonly=False)
     in_liquidation = fields.Boolean(string='In Liquidation', store=True, readonly=False)
