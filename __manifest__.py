5b9955f021adff030363782e841a23ea25562957
Fabian Krahtz <fabian.krahtz@live.com>
2020-06-22 14:20:17 +0000

    Update manifest and add icon

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_document",
+    'name': "Odoo Document",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Custom template for German business documents""",
 
     'description': """
-        Long description of module's purpose
+        TODO
     """,
 
     'author': "42 N.E.R.D.S.",
     'website': "https://www.42nerds.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['account', 'base'],
 
-    # always loaded
     'data': [
         'report/din5008_report.xml',
         'data/report_layout.xml',
         'views/res_company.xml',
     ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
