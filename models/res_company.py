0bd017d5dca1fc2f8aa9ab4b959e0d132b904309
wsoePhantom <defielp@gmail.com>
2020-09-21 14:07:40 +0000

    [IMP] German translation completed

--- models/res_company.py
+++ models/res_company.py
@@ -6,7 +6,7 @@ class Company(models.Model):
     ceos = fields.Char(string='CEOs', store=True, readonly=False)
     company_registry_id = fields.Char(string='Company Reg No', store=True, readonly=False)
     corporation_location = fields.Char(string='Location of Corporation', store=True, readonly=False)
-    bank_name = fields.Char(string='Name of the bank', store=True, readonly=False)
+    bank_name = fields.Char(string='Name of the Bank', store=True, readonly=False)
     bank_bic = fields.Char(string='BIC', store=True, readonly=False)
     bank_acc_number = fields.Char(string='IBAN', store=True, readonly=False)
     fax = fields.Char(string='Fax', store=True, readonly=False)
